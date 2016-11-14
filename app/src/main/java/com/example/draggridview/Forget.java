c3476598bf80bca08bbb157ead1c2e9384e83c9b
k19953014 <k19831107@gmail.com>
2016-11-14 14:24:29 +0800

    add startimage

--- app/src/main/java/com/example/draggridview/Forget.java
+++ app/src/main/java/com/example/draggridview/Forget.java
@@ -10,17 +10,17 @@ public class Forget extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.forgot);
+        setContentView(R.layout.activity_forget);
 
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                final Intent mainIntent = new Intent(Forget.this, MainActivity.class);
-//                startActivity(mainIntent);
-//                Forget.this.finish();
-//                // 當跳到另一 Activity 時，讓 MainActivity 結束。
-//                // 這樣可以避免使用者按 back 後，又回到該 activity。
-//            }
-//        }, 5000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(Forget.this, MainView.class);
+                startActivity(mainIntent);
+                Forget.this.finish();
+                // 當跳到另一 Activity 時，讓 MainActivity 結束。
+                // 這樣可以避免使用者按 back 後，又回到該 activity。
+            }
+        }, 2000);
     }
 }
